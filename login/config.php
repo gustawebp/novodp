98b498c6262023d99d5e064f39132d7c9f9aab1a
gustawebp <sacodepaodeleite@gmail.com>
2023-03-29 21:32:10 -0300

    PHP desconectado do DB - CSS PRONTO

---
+++ login/config.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$dbHpst = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'formphp';
+
+$conexao = new mysqli($dbHpst, $dbUsername, $dbPassword, $dbName);
+
+// if($conexao->connect_errno)
+//  {
+//     echo "erro";
+
+//  }
+// else {
+//     echo "deserro";
+// }
+
+
+?>
---
+++ login/iniciar.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once('config.php');
+error_reporting(0);
+
+if(isset($_POST['submit'])) {
+    // print_r($_POST['nome']);
+    // print_r('<br>');
+    // print_r($_POST['cpf']);
+    // print_r('<br>');
+    // print_r($_POST['datanasc']);
+    // print_r('<br>');
+    // print_r($_POST['sexo']);
+?>
